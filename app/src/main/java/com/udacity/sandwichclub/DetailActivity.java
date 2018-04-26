e6bb05d1d869edfc2d418f8b9592d7d2ede27c6a
TheFredward <fventura59@gmail.com>
2018-04-26 12:50:00 -0500

    initial commit

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -21,6 +24,8 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
+
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,11 +48,10 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
-
         setTitle(sandwich.getMainName());
     }
 
@@ -56,7 +60,26 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    /**
+     *
+     * @param sandwich class that contains the data to be used for populating the xml
+     *
+     */
+
+    private void populateUI(Sandwich sandwich) {
+        //create all TextViews and reference the xml
+        TextView mainNameTV = findViewById(R.id.main_name_tv);
+        TextView alsoKnownTV = findViewById(R.id.also_known_tv);
+        TextView ingredientsTV = findViewById(R.id.ingredients_tv);
+        TextView originTV = findViewById(R.id.origin_tv);
+        TextView descriptionTV = findViewById(R.id.description_tv);
+
+        //set all data based on the methods from Sandwich class
+        mainNameTV.setText(sandwich.getMainName());
+        alsoKnownTV.setText(sandwich.getAlsoKnownAs().toString());
+        ingredientsTV.setText(sandwich.getIngredients().toString());
+        originTV.setText(sandwich.getPlaceOfOrigin());
+        descriptionTV.setText(sandwich.getDescription());
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,57 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
+import com.udacity.sandwichclub.MainActivity;
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwichRequested = null;
+        try {
+            //Initialize opbject
+            JSONObject jsonObject = new JSONObject(json);
+
+            //call jsonobject and go into Object known as name
+            JSONObject sandwichDetails = jsonObject.getJSONObject("name");
+
+            //Now inside of name, get  proper keys, ie main name
+            String mainName = sandwichDetails.getString("mainName");
+            //For loop to iterate and populate the also known as details
+            JSONArray alsoKnown = sandwichDetails.getJSONArray("alsoKnownAs");
+            //Create a List to pass to Sandwich class
+            List<String> alsoKnownStringList = new ArrayList<>();
+            if (alsoKnown != null) {
+                for (int i = 0; i < alsoKnown.length(); i++) {
+                    alsoKnownStringList.add(alsoKnown.optString(i));
+                }
+            }
+
+            //Populate the rest with the proper jsonKey
+            String origin = jsonObject.getString("placeOfOrigin");
+            String description = jsonObject.getString("description");
+            String image = jsonObject.getString("image");
+            //Ingredients is an array so use a for loop
+            JSONArray ingredientsArray = jsonObject.getJSONArray("ingredients");
+            List<String> ingredientStringList = new ArrayList<>();
+            if (ingredientsArray != null) {
+                for (int i = 0; i < ingredientsArray.length(); i++) {
+                    ingredientStringList.add(ingredientsArray.getString(i));
+                }
+            }
+
+            sandwichRequested = new Sandwich(mainName, alsoKnownStringList, origin, description, image, ingredientStringList);
+        } catch (final JSONException e) {
+            Log.e(MainActivity.class.getSimpleName(), "JSON parsing error: " + e.getMessage());
+        }
+        return sandwichRequested;
     }
 }
